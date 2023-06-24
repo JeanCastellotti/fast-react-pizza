4e411eea31376708269d1e1d1cdfb71c4c2b57f5
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-06-24 11:16:29 +0200

    display loader

--- src/ui/Layout.jsx
+++ src/ui/Layout.jsx
@@ -1,10 +1,16 @@
 import Header from './header'
 import CartOverview from '../features/cart/CartOverview'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
+import Loader from './Loader'
 
 function Layout() {
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
