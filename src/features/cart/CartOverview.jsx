a58dc0ff26ec3723278d392ea9e2b7f21cd47dfe
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2023-06-26 09:54:29 +0200

    improve cart

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
 import { Link } from 'react-router-dom'
+import { getTotalPrice, getTotalQuantity } from './cartSlice'
+import { useSelector } from 'react-redux'
+import { formatCurrency } from '../../utils/helpers'
 
 function CartOverview() {
+  const totalQuantity = useSelector(getTotalQuantity)
+  const totalPrice = useSelector(getTotalPrice)
+
+  if (!totalQuantity) return
+
   return (
     <div className="flex justify-between bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalQuantity} pizzas</span>
+        <span>{formatCurrency(totalPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
